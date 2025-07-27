58bb4ae8a3788a18851584e89f989efa2fe06111
Fabio Calete <gersoncalete@gmail.com>
2025-07-27 05:47:20 +0200

    Fixed urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
-    path('contact/', views.contact, name'contact'),
+    path('contact/', views.contact, name='contact'),
 ]
 
 
