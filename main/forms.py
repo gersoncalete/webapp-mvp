54f1291f6dab13702c0ae201b52ed193449b4c72
Fabio Calete <gersoncalete@gmail.com>
2025-07-27 10:23:44 +0200

    Added custom css to form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+            'class': 'custom-input',
+            'placeholder': 'Your full name'
+        }))
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'custom-input', 
+            'placeholder': 'your.email@example.com'
+        }))
+    message = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'custom-textarea', 'placeholder': 'Enter your message here...'}
+    ))
